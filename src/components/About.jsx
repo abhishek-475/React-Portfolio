6cc33047aeafb6f1e6e74c31f4804cf3a0c29050
abhishek-475 <61498092+abhishek-475@users.noreply.github.com>
2023-10-15 10:35:51 +0530

    complete

---
+++ src/components/About.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div name="about"
+     className="w-full h-screen bg-gradient-to-b from-gray-800 to-black
+     text-white">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col
+       justify-center w-full h-full">
+        <div className="pb-8">
+            <p className="text-4xl font-bold inline border-b-4
+            border-gray-500">About</p>
+        </div>
+        <p className="text-xl mt-20">
+         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
+          deserunt illum mollitia officiis qui exercitationem perferendis neque
+          quasi a recusandae necessitatibus tempora iusto! Blanditiis error
+          iste, totam fugiat recusandae rerum laborum perferendis molestiae
+          aperiam asperiores nemo. Magni dolor maxime debitis vitae, eaque hic
+          ab mollitia voluptatibus, a nostrum eveniet laborum!
+        </p>
+        <br />
+        <p className="text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae modi placeat molestiae neque quibusdam, minima iure. Ea, eligendi dicta ut magnam cumque debitis neque praesentium impedit, perferendis molestiae laborum quia nisi. Officia ipsum, id quasi molestiae eos quia aspernatur dolorum dicta quas quaerat totam, impedit repellat praesentium nemo accusamus veniam.
+        </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
